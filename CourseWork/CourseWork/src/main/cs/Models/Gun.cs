4d149b376ebf5d6ab5adaf0e8baa3e8dd9c793df
YakubT <goldyakub@gmail.com>
2022-12-25 14:08:23 +0200

    I implented rotation of gun

---
+++ CourseWork/CourseWork/src/main/cs/Models/Gun.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork.src.main.cs.Models
+{
+    public class Gun
+    {
+        public double Angle { set; get; }
+
+        public void Shoot()
+        {
+
+        }
+    }
+}
---
+++ CourseWork/CourseWork/src/main/cs/ViewModels/FieldViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CourseWork.src.main.cs.ViewModels
+{
+    public class GunRotateCommand : ICommand
+    {
+        private FieldViewModel receiver;
+        
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public GunRotateCommand (FieldViewModel receiver)
+        {
+            this.receiver = receiver;
+        }
+        public void Execute(object parameter)
+        {
+            double tg = ((((MouseEventArgs)parameter).GetPosition(receiver.Window).Y -
+                receiver.Window.Height * 23 / 24) /
+                (((MouseEventArgs)parameter).GetPosition(receiver.Window).X - receiver.Window.Width * 16 / 24));
+            double angle = 180*Math.Atan(Math.Abs(tg))/Math.PI;
+            if (tg > 0)
+            {
+                angle = -90 + angle;
+            }
+            else
+                angle = 90 - angle;
+            if (angle>-60 && angle<50)
+            this.receiver.Angle = angle;
+           
+        }
+    }
+    public class FieldViewModel : BaseViewModel
+    {
+        private Window window;
+        public GunRotateCommand RotateGunCommand { get;}
+        
+        private double angle;
+        public double Angle
+        {
+            get => angle;
+            set
+            {
+                angle = value;
+                OnPropertyChanged(nameof(Angle));
+            }
+        }
+        public FieldViewModel(Window window)
+        {
+            this.window = window;
+            RotateGunCommand = new GunRotateCommand(this);
+
+        }
+
+        public Window Window { get => window; }
+
+       
+    }
+}
